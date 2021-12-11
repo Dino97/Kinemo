e5ea4f9da45955081c733c725cd517237e9443a6
Dino97 <dino.wow97@gmail.com>
2021-12-11 15:54:15 +0100

    using Kinemo::Texture2D type for framebuffer color attachment

--- engine/src/graphics/Framebuffer.cpp
+++ engine/src/graphics/Framebuffer.cpp
@@ -53,15 +53,11 @@ namespace Kinemo
     {
         CHECK_FRAMEBUFFER_BINDING();
 
-        unsigned int colorBufferHandle;
-        glGenTextures(1, &colorBufferHandle);
-        glBindTexture(GL_TEXTURE_2D, colorBufferHandle);
-        
-        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, m_Width, m_Height, 0, GL_RGB, GL_UNSIGNED_BYTE, nullptr);
+        m_ColorBuffer = std::make_unique<Texture2D>(m_Width, m_Height, 3, nullptr);
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 
-        glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, colorBufferHandle, 0);
+        glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, m_ColorBuffer->GetHandle(), 0);
 
         m_ClearFlags |= GL_COLOR_BUFFER_BIT;
     }
--- engine/src/graphics/Framebuffer.h
+++ engine/src/graphics/Framebuffer.h
@@ -3,6 +3,8 @@
 #include "Texture2D.h"
 #include "Color.h"
 
+#include <memory>
+
 namespace Kinemo
 {
     class Framebuffer
@@ -26,9 +28,13 @@ namespace Kinemo
         void CreateColorBuffer();
         void CreateDepthStencilBuffer();
 
+        const std::unique_ptr<Texture2D>& GetColorBuffer() const { return m_ColorBuffer; }
+        //const std::unique_ptr<Texture2D>& GetDepthStencilBuffer() const { return m_ColorBuffer; } // TO-DO
+
         bool IsComplete() const;
 
     private:
+        std::unique_ptr<Texture2D> m_ColorBuffer/*, m_DepthStencilBuffer*/;
         unsigned int m_Handle;
         unsigned int m_Width, m_Height;
         int m_ClearFlags;
